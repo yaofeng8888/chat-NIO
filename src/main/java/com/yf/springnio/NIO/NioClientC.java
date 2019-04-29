d3d630f0b21f1b11b69617d2eabd033dd6074a64
yaofeng8888 <461563387@qq.com>
2019-04-29 11:04:32 +0800

    chart-NIO

---
+++ src/main/java/com/yf/springnio/NIO/NioClientC.java
@@ -0,0 +1,12 @@
+package com.yf.springnio.NIO;
+
+/**
+ * @author: yaofeng
+ * @create:2019-04-29-10:58
+ **/
+public class NioClientC {
+    public static void main(String[] strings)throws Exception{
+        new NioClient().start("clentC");
+    }
+
+}
